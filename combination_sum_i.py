48c1899131d576b200625c316ec079992addacf0
yl737 <51252710+yl737@users.noreply.github.com>
2023-03-22 02:44:01 -0700

    Update combination_sum_i.py

--- combination_sum_i.py
+++ combination_sum_i.py
@@ -9,18 +9,16 @@ class Solution(object):
         def dfs(cand, t, i, temp, ans):
             if i == len(cand) or cand[i] > t:
                 return
-            if t == 0:
+            if cand[i] == t:
+                temp.append(cand[i])
                 ans.append(temp[:])
-            #if cand[i] == t:
-            #    temp.append(cand[i])
-            #    ans.append(temp[:])
-            #    temp.pop()
+                temp.pop()
             temp.append(cand[i])
-            dfs(cand,t-cand[i], i, temp, ans)
-            dfs(cand,t-cand[i], i+1, temp, ans)
+            dfs(cand,t-cand[i], i, temp, ans) #  take cand[i] not move on 
+            #dfs(cand,t-cand[i], i+1, temp, ans)
             
             temp.pop()
-            dfs(cand,t, i+1, temp, ans)
+            dfs(cand,t, i+1, temp, ans) #  not take cand[i] and move on 
         temp, ans = [], []
         dfs(candidates, target, 0, temp, ans)
         return ans
